721bee9ca428e3f97b49628da6b0305002add877
maldikurniawan <akurniawan2102@gmail.com>
2024-06-28 16:59:57 +0700

    add map

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{scrollBehavior:'smooth'}}>
       <body className="bg-[#F1F1F1]">{children}</body>
     </html>
   );
